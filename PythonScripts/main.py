5e5a32577609b0971a2ba3f71c899412bf1a3378
Якушевский Артём <artem19o@yandex.ru>
2023-02-08 00:31:46 +0500

    measure_time

--- PythonScripts/main.py
+++ PythonScripts/main.py
@@ -1,3 +1,13 @@
+import timeit
 import sorts_comparison
 
-sorts_comparison.print_list([1, 2, 3])
+
+def measure_time(func, data):
+    start_time = timeit.default_timer()
+    func(data)
+    time_of_ex = timeit.default_timer() - start_time
+    return time_of_ex
+
+
+data = [5, 9, 3, 2, 1, 8, 7, 4, 0, 6]
+print(measure_time(sorts_comparison.selection_sort, data))
--- SortsComparison/main.cpp
+++ SortsComparison/main.cpp
@@ -1,17 +1,28 @@
 ﻿#include "SortsComparison.h"
 
-float sum(float arg1, float arg2) {
-    return arg1 + arg2;
-}
-
-void printList(vector<float> list) {
+void printList(const vector<int> &list) {
     for (auto item : list) {
         std::cout << item << ' ';
     }
     std::cout << std::endl;
 }
 
+void selectionSort(vector<int> &data) {
+    for (int i = 0; i < data.size(); i++) {
+        int min = i;
+        for (int j = i + 1; j < data.size(); j++) {
+            min = (data[j] < data[min]) ? j : min;
+        }
+
+        if (i != min) {
+            int temp = data[i];
+            data[i] = data[min];
+            data[min] = temp;
+        }
+    }
+}
+
 PYBIND11_MODULE(sorts_comparison, module_handle) {
-    module_handle.def("sum", &sum);
+    module_handle.def("selection_sort", &selectionSort);
     module_handle.def("print_list", &printList);
 }
