d28a84160c276941cb9513ba2b06607753d4c0c5
Якушевский Артём <artem19o@yandex.ru>
2023-02-08 01:00:18 +0500

    multi_measure_performance

--- PythonScripts/main.py
+++ PythonScripts/main.py
@@ -1,13 +1,27 @@
-import timeit
 import sorts_comparison
+import timeit
+
+TIMES_TO_RUN = 100
 
 
-def measure_time(func, data):
+def measure_performance(func, argument):
     start_time = timeit.default_timer()
-    func(data)
-    time_of_ex = timeit.default_timer() - start_time
-    return time_of_ex
+    operations_quantity = func(argument)
+    time_of_execution = timeit.default_timer() - start_time
+    return operations_quantity, time_of_execution
+
+
+def multi_measure_performance(func, argument):
+    operations_quantity_data = list()
+    time_of_execution_data = list()
+    for _ in range(TIMES_TO_RUN):
+        oq, toe = measure_performance(func, argument)
+        operations_quantity_data.append(oq)
+        time_of_execution_data.append(toe)
+    operations_quantity = sum(operations_quantity_data) / len(operations_quantity_data)
+    time_of_execution = sum(time_of_execution_data) / len(time_of_execution_data)
+    return operations_quantity, time_of_execution
 
 
 data = [5, 9, 3, 2, 1, 8, 7, 4, 0, 6]
-print(measure_time(sorts_comparison.selection_sort, data))
+print(multi_measure_performance(sorts_comparison.selection_sort, data))
--- SortsComparison/main.cpp
+++ SortsComparison/main.cpp
@@ -7,19 +7,25 @@ void printList(const vector<int> &list) {
     std::cout << std::endl;
 }
 
-void selectionSort(vector<int> &data) {
-    for (int i = 0; i < data.size(); i++) {
+int selectionSort(vector<int> &data) {
+    int oc = 0; // operations counter
+    ++oc;
+    for (int i = 0; i < data.size(); ++i) {
         int min = i;
-        for (int j = i + 1; j < data.size(); j++) {
+        oc += 6;
+        for (int j = i + 1; j < data.size(); ++j) {
             min = (data[j] < data[min]) ? j : min;
+            oc += 8;
         }
-
+        oc += 2;
         if (i != min) {
             int temp = data[i];
             data[i] = data[min];
             data[min] = temp;
+            oc += 7;
         }
     }
+    return oc;
 }
 
 PYBIND11_MODULE(sorts_comparison, module_handle) {
