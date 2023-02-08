7584099b5b47660bbe16529a1713dab9ecada00b
Якушевский Артём <artem19o@yandex.ru>
2023-02-09 00:35:16 +0500

    Скрипт готов

---
+++ PythonScripts/data_generator.py
@@ -0,0 +1,18 @@
+import random
+
+
+def generate_data(array_max_size, change_step):
+    zero_to_5 = [random.randint(0, 5) for _ in range(array_max_size)]
+    zero_to_4000 = [random.randint(0, 4000) for _ in range(array_max_size)]
+    reversed_list = [i for i in range(4100, 4100 - array_max_size, -1)]
+    almost_sorted = [i for i in range(array_max_size)]
+    i = 0
+    while i + change_step < array_max_size:
+        almost_sorted[i], almost_sorted[i + change_step] = almost_sorted[i + change_step], almost_sorted[i]
+        i += 2 * change_step
+    return {
+        'zero_to_5': zero_to_5,
+        'zero_to_4000': zero_to_4000,
+        'almost_sorted': almost_sorted,
+        'reversed_list': reversed_list,
+    }
---
+++ PythonScripts/sorts_performance_calculator.py
@@ -0,0 +1,71 @@
+import sorts_comparison
+import json
+import timeit
+from itertools import chain
+from data_generator import generate_data
+
+TIMES_TO_RUN = 10
+ARRAY_MAX_SIZE = 4100
+CHANGE_STEP = 50
+DATA = generate_data(ARRAY_MAX_SIZE, CHANGE_STEP)
+SORTS = {
+    'selection_sort': sorts_comparison.selection_sort
+}
+
+
+def helper3(func, argument, expected_result):
+    start_time = timeit.default_timer()
+    result = func(argument)
+    time_of_execution = timeit.default_timer() - start_time
+    if result[0] != expected_result:
+        raise RuntimeError('Массив отсортирован неверно!')
+    return result[1], time_of_execution
+
+
+def helper2(func, array):
+    expected_result = array.copy()
+    expected_result.sort()
+
+    operations_quantity_data = list()
+    time_of_execution_data = list()
+    for _ in range(TIMES_TO_RUN):
+        oq, toe = helper3(func, array, expected_result)
+        operations_quantity_data.append(oq)
+        time_of_execution_data.append(toe)
+
+    operations_quantity = sum(operations_quantity_data) / len(operations_quantity_data)
+    time_of_execution = sum(time_of_execution_data) / len(time_of_execution_data) / 1e-9
+
+    return {
+        'operations': int(operations_quantity),
+        'time_ns': time_of_execution
+    }
+
+
+def helper1(sort_name):
+    func = SORTS[sort_name]
+    result = dict()
+    for array_name in DATA.keys():
+        array = DATA[array_name]
+        result[array_name] = dict()
+        for i in chain(range(50, 300, 50), range(300, 4100 + 1, 100)):
+            print(f'{sort_name} -> {array_name} -> {i}')
+            result[array_name][i] = helper2(func, array[0:i])
+    return result
+
+
+def calculate_sorts_performance():
+    result = dict()
+    for sort_name in SORTS.keys():
+        result[sort_name] = helper1(sort_name)
+    return result
+
+
+def main():
+    sorts_performance = calculate_sorts_performance()
+    with open('sorts_performance_result.json', 'w') as outfile:
+        json.dump(sorts_performance, outfile, indent=2)
+
+
+if __name__ == '__main__':
+    main()
